ed52570226c52216ca0f1f7bf9ce0ef0dc3fecd2
Rupam <rupamkarmakar1238@gmail.com>
2022-08-01 01:06:29 +0530

    BinarySearch Committed

--- DSA/searching.cpp
+++ DSA/searching.cpp
@@ -1,6 +1,30 @@
 #include<iostream>
 using namespace std;
 
+int binarySearch(int key, int arr[], int n)
+{
+    int s = 0;
+    int e = n;
+    while (s<=e)
+    {
+        int mid = (s+e)/2;
+        if(arr[mid] == key)
+        {
+            return mid;
+        }
+        else if(arr[mid] > key)
+        {
+            e = mid - 1;
+        }
+        else
+        {
+            s = mid + 1;
+        }
+    }
+    cout<<"Element not found"<<endl;
+    return -1;
+}
+
 int linearSearch(int key, int arr[], int n)
 {
     for (int i = 0; i < n; i++)
@@ -12,6 +36,7 @@ int linearSearch(int key, int arr[], int n)
         }
                 
     }
+    cout<<"Element not found"<<endl;
     return -1;
 }
 
@@ -31,6 +56,12 @@ int main(){
         cout<<"Please enter the number you want to search = ";
         cin>>key;
         cout<<linearSearch(key, arr, arrSize)<<endl;
+        break;
+
+    case 'B':
+        cout<<"Please enter the number you want to search = ";
+        cin>>key;
+        cout<<binarySearch(key, arr, arrSize)<<endl;
         break;
     
     default:
