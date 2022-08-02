08aba9eec8e9f84d3a77ff9ebf600a7937d6a433
RUPAM <rupamkarmakar1238@gmail.com>
2022-08-02 21:57:57 +0530

    maxSumArrayEles committed2

--- DSA/maxSubArr.cpp
+++ DSA/maxSubArr.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
+#include <climits>
+
 using namespace std;
 
 void maxSubArrs(int arr[], int n)
 {
+    int maxSum = INT_MIN;
     for (int i = 0; i < n; i++)
     {
         for (int j = i; j < n; j++)
         {
+            int sum = 0;
             for (int k = i; k <= j; k++)
             {
-                cout << arr[k] << " ";
+                sum += arr[k];
             }
-            cout << endl;
+
+            maxSum = max(maxSum, sum);
         }
     }
+    cout << maxSum << endl;
 }
 
 int main()
