e877db625aee7c61585f15109febf79a95d182e8
Rupam <rupamkarmakar1238@gmail.com>
2022-08-15 00:59:25 +0530

    stackApplication committed

--- DSA/stackApplication.cpp
+++ DSA/stackApplication.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <stack>
+#include <math.h>
+#include <algorithm>
 using namespace std;
 
 void reverseSentence(string s)
@@ -65,6 +67,273 @@ void reverse(stack<int> &st)
     insertAtBottom(st, ele);
 }
 
+int prefixEvaluation(string s)
+{
+    stack<int> st;
+
+    for (int i = s.length() - 1; i >= 0; i--)
+    {
+        if (s[i] >= '0' && s[i] <= '9')
+        {
+            st.push(s[i] - '0');
+        }
+        else
+        {
+            int op1 = st.top();
+            st.pop();
+            int op2 = st.top();
+            st.pop();
+
+            switch (s[i])
+            {
+            case '+':
+                st.push(op1 + op2);
+                break;
+
+            case '-':
+                st.push(op1 - op2);
+                break;
+
+            case '*':
+                st.push(op1 * op2);
+                break;
+
+            case '/':
+                st.push(op1 / op2);
+                break;
+
+            case '^':
+                st.push(pow(op1, op2));
+                break;
+            }
+        }
+    }
+
+    return st.top();
+}
+
+int postfixEvaluation(string s)
+{
+    stack<int> st;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] >= '0' && s[i] <= '9')
+        {
+            st.push(s[i] - '0');
+        }
+        else
+        {
+            int op2 = st.top();
+            st.pop();
+            int op1 = st.top();
+            st.pop();
+
+            switch (s[i])
+            {
+            case '+':
+                st.push(op1 + op2);
+                break;
+
+            case '-':
+                st.push(op1 - op2);
+                break;
+
+            case '*':
+                st.push(op1 * op2);
+                break;
+
+            case '/':
+                st.push(op1 / op2);
+                break;
+
+            case '^':
+                st.push(pow(op1, op2));
+                break;
+            }
+        }
+    }
+
+    return st.top();
+}
+
+int prec(char c)
+{
+    if (c == '^')
+    {
+        return 3;
+    }
+    else if (c == '*' || c == '/')
+    {
+        return 2;
+    }
+    else if (c == '+' && c == '-')
+    {
+        return 1;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+string infixToPostfix(string s)
+{
+    stack<char> st;
+    string res;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+        {
+            res += s[i];
+        }
+        else if (s[i] == '(')
+        {
+            st.push(s[i]);
+        }
+        else if (s[i] == ')')
+        {
+            while (!st.empty() && st.top() != '(')
+            {
+                res += st.top();
+                st.pop();
+            }
+
+            if (!st.empty())
+            {
+                st.pop();
+            }
+        }
+        else
+        {
+            while (!st.empty() && prec(st.top()) > prec(s[i]))
+            {
+                res += st.top();
+                st.pop();
+            }
+            st.push(s[i]);
+        }
+    }
+
+    while (!st.empty())
+    {
+        res += st.top();
+        st.pop();
+    }
+
+    return res;
+}
+
+string infixToPrefix(string s)
+{
+    reverse(s.begin(), s.end());
+
+    stack<char> st;
+    string res;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+        {
+            res += s[i];
+        }
+        else if (s[i] == ')')
+        {
+            st.push(s[i]);
+        }
+        else if (s[i] == '(')
+        {
+            while (!st.empty() && st.top() != ')')
+            {
+                res += st.top();
+                st.pop();
+            }
+
+            if (!st.empty())
+            {
+                st.pop();
+            }
+        }
+        else
+        {
+            while (!st.empty() && prec(st.top()) >= prec(s[i]))
+            {
+                res += st.top();
+                st.pop();
+            }
+            st.push(s[i]);
+        }
+    }
+
+    while (!st.empty())
+    {
+        res += st.top();
+        st.pop();
+    }
+
+    reverse(res.begin(), res.end());
+    return res;
+}
+
+bool isBalanced(string s)
+{
+    int n = s.size();
+
+    stack<char> st;
+    bool ans = true;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] == '{' or s[i] == '(' or s[i] == '[')
+        {
+            st.push(s[i]);
+        }
+        else if (s[i] == ')')
+        {
+            if (!st.empty() and st.top() == '(')
+            {
+                st.pop();
+            }
+            else
+            {
+                ans = false;
+                break;
+            }
+        }
+        else if (s[i] == '}')
+        {
+            if (!st.empty() and st.top() == '{')
+            {
+                st.pop();
+            }
+            else
+            {
+                ans = false;
+                break;
+            }
+        }
+        else if (s[i] == ']')
+        {
+            if (!st.empty() and st.top() == '[')
+            {
+                st.pop();
+            }
+            else
+            {
+                ans = false;
+                break;
+            }
+        }
+    }
+
+    if (!st.empty())
+    {
+        return false;
+    }
+    return ans;
+}
+
 int main()
 {
     string s = "Hey, how are you doing";
@@ -79,6 +348,24 @@ int main()
 
     reverse(st);
     displayStack(st);
+
+    cout << "-+7*45+20 = " << prefixEvaluation("-+7*45+20") << endl;
+
+    cout << "46+2/5*7+ = " << postfixEvaluation("46+2/5*7+") << endl;
+
+    cout << "(a-b/c)*(a/k-l) = " << infixToPostfix("(a-b/c)*(a/k-l)") << endl;
+
+    cout << "(a-b/c)*(a/k-l) = " << infixToPrefix("(a-b/c)*(a/k-l)") << endl;
+
+    s = "({[]}";
+    if (isBalanced(s))
+    {
+        cout << "Balanced string" << endl;
+    }
+    else
+    {
+        cout << "Non Balanced string" << endl;
+    }
 
     return 0;
 }
